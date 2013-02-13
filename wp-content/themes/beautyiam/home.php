5b7862e291d0e704c4b3c9b9ff83558f8e644f2c
Jesse Kade <jessekade@Sharp-Machine.local>
2013-02-13 14:00:40 -0800

    started on landing page

---
+++ wp-content/themes/beautyiam/home.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<section id="page">
+
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<div class="item active">
+				<img src="<?php bloginfo('template_directory'); ?>/img/1.jpeg" alt="">
+			</div>
+			<div class="item">
+				<img src="<?php bloginfo('template_directory'); ?>/img/2.jpeg" alt="">
+			</div>
+		</div>
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+	</div>
+	
+	<div class="landing">
+		<h1>Hello</h1>
+	</div>
+	
+</section><!-- #page -->
+ 	<div class="container">
+			<div class="row">
+<?php get_footer(); ?>
