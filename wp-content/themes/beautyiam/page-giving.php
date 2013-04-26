f0dfe525cd5b37fb55ad2cfe26c1ef2d5f43acc7
Jesse Kade <jessekade@Sharp-Machine.local>
2013-04-26 16:33:29 -0700

    finished styling giving page

---
+++ wp-content/themes/beautyiam/page-giving.php
@@ -0,0 +1,67 @@
+<?php get_header(); ?>
+
+<section id="giving" class="page">
+	<div class="container">
+		<div class="row">
+		
+			<div class="span12">
+				<h1 class="serif">Partner with us <span class="sans">Transforming Hollywood</span></h1>
+			</div>
+			
+		</div>
+		
+		<div class="row">
+			<div class="span5">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium omnis harum velit quasi eaque at dolorem reiciendis culpa repellat expedita deserunt delectus corporis fuga impedit quaerat placeat deleniti quia repudiandae ab blanditiis mollitia qui enim!</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis sit quis porro sequi labore excepturi possimus nihil aliquid! Illum perspiciatis distinctio nesciunt veritatis dicta voluptatum deserunt laboriosam deleniti aperiam quibusdam consectetur commodi asperiores.</p>
+			</div>
+			<div class="span6 offset1">
+				<div id="monthly-partners" class="row">
+					<div class="span6">
+						<h6 class="serif">Monthly</h6>
+						<a href="#" class="btn btn-purple btn-donate"><span class="line2">$10</span></a>
+						<a href="#" class="btn btn-purple btn-donate"><span class="line2">$20</span></a>
+						<a href="#" class="btn btn-purple btn-donate"><span class="line2">$50</span></a>
+						<a href="#" class="btn btn-purple btn-donate"><span class="line2">$100</span></a>
+					</div>
+				</div>
+				<div id="one-time" class="row">
+					<div class="span5">
+						<h6 class="serif">Once-off</h6>
+						<form action="">
+							<input type="text" placeholder="Amount">
+							<button class="btn btn-purple btn-submit">Proceed</button>
+						</form>
+					</div>
+				</div>
+				
+				
+			</div>
+		</div>
+		<hr>
+		
+		<div id="payment-methods" class="row">
+			<div class="span5 pull-right">
+				<img src="<?php bloginfo( 'template_directory' ); ?>/img/paypal.png" alt="Paypal Logos">
+				<img src="<?php bloginfo( 'template_directory' ); ?>/img/credit-cards.png" alt="Credit Cards Logos">
+			</div>
+		</div>
+	</div>
+	
+	<div class="page-footer">
+		<div class="container">
+			<div class="row">
+				<div class="span7 tagline">
+					<h4 class="serif">Beauty <span class="i-am">i am</span> has shown me what I am capable of. Watch out Hollywood!</h4>
+				</div>
+				<div class="span2 curly-bracket curly-bracket-white"></div>
+				<div class="span3 featurette">
+					<div class="feature-title">Featured Artist</div>
+					<div class="feature-name serif">Frank Valentine</div>
+					<a href="#" class="btn btn-purple">See Full Interview</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section><!-- #page -->
+<?php get_footer(); ?>
