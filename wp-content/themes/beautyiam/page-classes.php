7f2789156626789eb894cb5aff6db4078b415c6d
Jesse Kade <jessekade@Sharp-Machine.local>
2013-05-17 10:35:16 -0700

    added book class buttons on classes page as per Ashtons request

--- wp-content/themes/beautyiam/page-classes.php
+++ wp-content/themes/beautyiam/page-classes.php
@@ -54,6 +54,14 @@
 					<div class="span9 class-instructor-name">
 						Instructor \ <?php the_field('class_instructors_name'); ?>
 					</div>
+					<div class="span3 add-to-calendar">
+						<a href="mailto:info@beautyiam.org?subject=Book <?php the_title(); ?>" class="btn">
+							<span class="icon"></span>
+							<span class="btn-label">Book
+								<span class="line2">Class</span>
+							</span>
+						</a>
+					</div>
 				</div>
 				
 				<div class="row">
@@ -85,6 +93,14 @@
 					<div class="span9 class-instructor-name">
 						Instructor \ <?php the_field('class_instructors_name'); ?>
 					</div>
+					<div class="span3 add-to-calendar">
+						<a href="mailto:info@beautyiam.org?subject=Book <?php the_title(); ?>" class="btn">
+							<span class="icon"></span>
+							<span class="btn-label">Book
+								<span class="line2">Class</span>
+							</span>
+						</a>
+					</div>
 				</div>
 				
 				<div class="row">
@@ -115,6 +131,14 @@
 				<div class="row class-instructor">
 					<div class="span9 class-instructor-name">
 						Instructor \ <?php the_field('class_instructors_name'); ?>
+					</div>
+					<div class="span3 add-to-calendar">
+						<a href="mailto:info@beautyiam.org?subject=Book <?php the_title(); ?>" class="btn">
+							<span class="icon"></span>
+							<span class="btn-label">Book
+								<span class="line2">Class</span>
+							</span>
+						</a>
 					</div>
 				</div>
 				
